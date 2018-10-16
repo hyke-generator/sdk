921e5c353f923bf6114eb26bcca402d98c5c51de
Luka Klacar <lukaklacar@gmail.com>
2018-10-16 20:33:52 +0200

    Fixes

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,21 +1,19 @@
-import BaseGenerator from './generators/BaseGenerator'
-import TemplateGenerator, {
-  ITemplateGeneratorArgs
-} from './generators/TemplateGenerator'
-import { execute } from './util/execute'
-import { mkdir } from 'fs'
-import { getHikeDirectory, getNodeModulesDirectory } from './util/paths'
-import { writeToFile } from './util/file'
-import { addPackage } from './util/package'
+import BaseGenerator from "./generators/BaseGenerator";
+import TemplateGenerator, { ITemplateGeneratorArgs } from "./generators/TemplateGenerator";
+import { execute } from "./util/execute";
+import { mkdir } from "fs";
+import { getHikeDirectory, getNodeModulesDirectory } from "./util/paths";
+import { writeToFile } from "./util/file";
+import { addPackage } from "./util/package";
 
 export {
-  BaseGenerator,
-  TemplateGenerator,
-  ITemplateGeneratorArgs,
-  execute,
-  mkdir,
-  writeToFile,
-  getHikeDirectory,
-  getNodeModulesDirectory,
-  addPackage
-}
+    BaseGenerator,
+    TemplateGenerator,
+    ITemplateGeneratorArgs,
+    execute,
+    mkdir,
+    writeToFile,
+    getHikeDirectory,
+    getNodeModulesDirectory,
+    addPackage,
+};

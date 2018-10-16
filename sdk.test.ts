36099ece1f20150feab7d4afe92b03c88a856eca
Luka Klacar <lukaklacar@gmail.com>
2018-10-16 21:54:04 +0200

    feat(test): fix test

--- sdk.test.ts
+++
@@ -1,7 +0,0 @@
-import * as core from "../src/sdk";
-
-describe("SDK test", () => {
-    it("SDK is an object", () => {
-        expect(typeof core).toBe("object");
-    });
-});
--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -1,2 +1,7 @@
+import * as core from "../src/sdk";
+
 describe("SDK test", () => {
+    it("SDK is an object", () => {
+        expect(typeof core).toBe("object");
+    });
 });

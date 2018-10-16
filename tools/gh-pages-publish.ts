7a5f091210b0a8892c2a8b814b7b43e347bbd525
Luka Klacar <lukaklacar@gmail.com>
2018-10-16 21:44:28 +0200

    feat(semantic): Added semantic release

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -23,7 +23,7 @@ touch(".nojekyll")
 exec("git init")
 exec("git add .")
 exec('git config user.name "Luka Klacar"')
-exec('git config user.email "l.klacar@vegaitsourcing.rs"')
+exec('git config user.email "lukaklacar@gmail.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
